0d7cb1685d518df814c484d5d6e6212a14712c6f
frodrish <frodrish@gmail.com>
2025-10-16 20:35:34 -0400

    https://github.com/withastro/starlight/discussions/3461

--- examples/nofontmatter/src/content.config.ts
+++ examples/nofontmatter/src/content.config.ts
@@ -1,6 +1,22 @@
 import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
 import { docsLoader } from '@astrojs/starlight/loaders';
+import { docsSchema } from '@astrojs/starlight/schema';
+
+const docs = defineCollection({
+    loader: glob({ pattern: '**/*.(md|mdx)', base: './src/content/docs' }),
+    schema: null,
+})
+
+const docs2 =  defineCollection({
+    loader: docsLoader(),
+    schema: (context) =>
+        z.preprocess(
+            (frontmatter) => ({ title: '', ...frontmatter }),
+            docsSchema()(context)
+        ),
+})
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: null }),
+ docs
 };
